5d7f4aced64999231a05fafcd08c97de065dcf9e
piaoxue-181 <924592920@qq.com>
2025-07-19 09:09:04 +0800

    第二版

--- src/config.ts
+++ src/config.ts
@@ -2,7 +2,7 @@ export default {
   // 网站标题
   Title: '末雨乘风博客',
   // 网站地址
-  Site: 'https://www.blowswind.cn',
+  Site: 'https://blog.blowswind.cn',
   // 网站副标题
   Subtitle: '不曾与你分享的时间,我在进步.',
   // 网站描述
