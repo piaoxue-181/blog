3b18200a05c74d1dded563bad1344159720618fa
piaoxue-181 <924592920@qq.com>
2025-08-13 20:25:13 +0800

    修复

--- src/config.ts
+++ src/config.ts
@@ -1,16 +1,16 @@
 export default {
   // 网站标题
-  Title: '韩小韩博客',
+  Title: '末雨乘风博客',
   // 网站地址
-  Site: 'https://www.vvhan.com',
+  Site: 'https://blog.blowswind.cn',
   // 网站副标题
-  Subtitle: '不曾与你分享的时间,我在进步.',
+  Subtitle: '天上的星光漫漫，那是被夸父打碎的太阳.',
   // 网站描述
-  Description: '韩小韩博客 专注于前开发与相关技术的实战分享，涵盖Vue框架、Node.js、Serverless等，并涉及Node、Python、Linux、Docker等领域。同时，博客也分享作者的生活、音乐和旅行的热爱。',
+  Description: '末雨乘风博客 专注于前开发与相关技术的实战分享，涵盖Vue框架、Node.js、Serverless等，并涉及Node、Python、Linux、Docker等领域。同时，博客也分享作者的生活、音乐和旅行的热爱。',
   // 网站作者
-  Author: '.𝙃𝙖𝙣',
+  Author: '.𝙈𝗼𝘆𝙪𝙘𝙛',
   // 作者头像
-  Avatar: 'https://q1.qlogo.cn/g?b=qq&nk=1655466387&s=640',
+  Avatar: 'https://q1.qlogo.cn/g?b=qq&nk=924592920&s=640',
   // 网站座右铭
   Motto: '运气是计划之外的东西.',
   // Cover 网站缩略图
@@ -19,11 +19,11 @@ export default {
   Tips: '<p>欢迎光临我的博客 🎉</p><p>这里会分享我的日常和学习中的收集、整理及总结，希望能对你有所帮助:) 💖</p>',
   // 首页打字机文案列表
   TypeWriteList: [
-    '不曾与你分享的时间,我在进步.',
-    "I am making progress in the time I haven't shared with you.",
+    '天上的星光漫漫，那是被夸父打碎的太阳.',
+    "The starlight in the sky is vast and boundless, which is the sun shattered by Kuafu.",
   ],
   // 网站创建时间
-  CreateTime: '2021-09-01',
+  CreateTime: '2025-06-16',
   // 顶部 Banner 配置
   HomeBanner: {
     enable: true,
@@ -32,7 +32,7 @@ export default {
     // 其他页面高度
     PageHeight: '28.88rem',
     // 背景
-    background: "url('/assets/images/home-banner.webp') no-repeat center 60%/cover",
+    background: "url('https://image.blowswind.cn/v2/G7LTuLu.jpeg') no-repeat center 60%/cover",
   },
   // 博客主题配置
   Theme: {
@@ -58,17 +58,17 @@ export default {
     { text: '昔日', link: '/archives', icon: 'Nav_archives' },
     { text: '留言', link: '/message', icon: 'Nav_message' },
     { text: '关于', link: '/about', icon: 'Nav_about' },
-    { text: 'API', link: 'https://api.vvhan.com/', target: true, icon: 'Nav_link' },
+    { text: 'API', link: 'https://api.blowswind.cn/', target: true, icon: 'Nav_link' },
   ],
   // 侧边栏个人网站
   WebSites: [
     // 仅支持 SVG 且 SVG 需放在 public/assets/images/svg/ 目录下，填入文件名即可 <不需要文件后缀名>（封装了 SVG 组件 为了极致压缩 SVG）
     // 建议使用 https://tabler.io/icons 直接下载 SVG
-    { text: 'Github', link: 'https://github.com/uxiaohan', icon: 'WebSite_github' },
-    { text: '韩小韩API', link: 'https://api.vvhan.com', icon: 'WebSite_api' },
-    { text: '每日热榜', link: 'https://hot.vvhan.com', icon: 'WebSite_hot' },
-    { text: '骤雨重山图床', link: 'https://wp-cdn.4ce.cn', icon: 'WebSite_img' },
-    { text: 'HanAnalytics', link: 'https://analytics.vvhan.com', icon: 'WebSite_analytics' },
+    { text: 'Github', link: 'https://github.com/piaoxue-181', icon: 'WebSite_github' },
+    { text: '末雨乘风API', link: 'https://api.blowswind.cn', icon: 'WebSite_api' },
+    { text: '每日热榜', link: 'https://hot.blowswind.cn', icon: 'WebSite_hot' },
+    { text: '末雨乘风图床', link: 'https://image.blowswind.cn', icon: 'WebSite_img' },
+    { text: 'HanAnalytics', link: 'https://analytics.blowswind.cn', icon: 'WebSite_analytics' },
   ],
   // 侧边栏展示
   AsideShow: {
@@ -85,13 +85,13 @@ export default {
   DNSOptimization: [
     'https://i0.wp.com',
     'https://cn.cravatar.com',
-    'https://analytics.vvhan.com',
-    'https://vh-api.4ce.cn',
-    'https://registry.npmmirror.com',
-    'https://pagead2.googlesyndication.com'
+    'https://analytics.blowswind.cn',
+    'https://image.blowswind.cn',
+    'https://music-api.blowswind.cn',
+    'https://waline.blowswind.cn'
   ],
   // 博客音乐组件解析接口
-  vhMusicApi: 'https://vh-api.4ce.cn/blog/meting',
+  vhMusicApi: 'https://music-api.blowswind.cn/api',
   // 评论组件（只允许同时开启一个）
   Comment: {
     // Twikoo 评论
@@ -101,19 +101,19 @@ export default {
     },
     // Waline 评论
     Waline: {
-      enable: false,
-      serverURL: ''
+      enable: true,
+      serverURL: 'https://waline.blowswind.cn'
     }
   },
   // Han Analytics 统计（https://github.com/uxiaohan/HanAnalytics）
-  HanAnalytics: { enable: true, server: 'https://analytics.vvhan.com', siteId: 'Hello-HanHexoBlog' },
+  HanAnalytics: { enable: true, server: 'https://analytics.blowswind.cn', siteId: 'Hello-MoyucfHexoBlog' },
   // Google 广告
   GoogleAds: {
     ad_Client: '', //ca-pub-xxxxxx
     // 侧边栏广告(不填不开启)
-    asideAD_Slot: `<ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-xxxxxx" data-ad-slot="xxxxxx" data-ad-format="auto" data-full-width-responsive="true"></ins>`,
+    asideAD_Slot: ``,
     // 文章页广告(不填不开启)
-    articleAD_Slot: `<ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-xxxxxx" data-ad-slot="xxxxxx" data-ad-format="auto" data-full-width-responsive="true"></ins>`
+    articleAD_Slot: ``
   },
   // 文章内赞赏码
   Reward: {
